490123baa92cae2cc8458fb6a08cead539df62e3
Шевченко Андрей 519ст <84464140+shevchenko-andrey@users.noreply.github.com>
2021-10-03 16:26:40 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,8 +1,12 @@
 bot_name = "Julia"
 birth_year = "2021"
 
-print("Hello! My name is" + bot_name + "."" I was created in " + birth_year)
+print("Hello! My name is" + bot_name + "."" I was created in " + birth_year + ".")
 
+print("Please, remind me your name.")
+print(">")
+your_name = input()
+print("What a great name you have," + your_name + "!")
 
 
 
