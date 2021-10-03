ba844f38f7d9352ee0eaa21305d081b33ab47762
Шевченко Андрей 519ст <84464140+shevchenko-andrey@users.noreply.github.com>
2021-10-03 21:50:18 +0300

    ChatBot 3-rd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,12 +1,22 @@
 bot_name = "Julia"
 birth_year = "2021"
-
-print("Hello! My name is" + bot_name + "."" I was created in " + birth_year + ".")
-
+#1
+print("Hello! My name is " + bot_name + "." + " I was created in " + birth_year + ".")
+#2
 print("Please, remind me your name.")
-print(">")
-your_name = input()
-print("What a great name you have," + your_name + "!")
+your_name = input("> ")
+print("What a great name you have, " + your_name + "!")
+#3
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3, 5 and 7.")
+remainder3 = int(input("> "))
+remainder5 = int(input("> "))
+remainder7 = int(input("> "))
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print("Your age isп", age, ";", "that's a good time to start programming!")
+
+
+
 
 
 
