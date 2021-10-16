5a8394b085cef15e42267c85ec4a9f2a716fd96d
shevchenkoAndriy <s.s.student.khai.edu>
2021-10-16 17:37:26 +0300

    hangman 2-stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,7 +1,18 @@
+word = "train"
 def hangman_greeting():
-    print("HANGMAN")
+    print("HANGMAN.")
     print("The game will be available soon.")
 
 
-hangman_greeting()
+def simplified_version():
+    print("HANGMAN.")
+    print("Guess the word: >", word)
+    answer = (input("> ")).lower()
+    if answer == word:
+        print("You survived!")
+    else:
+        print("You lost!")
 
+
+hangman_greeting()
+simplified_version()
