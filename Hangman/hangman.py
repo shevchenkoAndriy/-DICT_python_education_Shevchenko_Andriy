37bd4b1d4ee11256cf6971dc1870b889da2d9ebc
shevchenkoAndriy <s.s.student.khai.edu>
2021-10-16 20:04:19 +0300

    hangman 3-stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,18 +1,43 @@
-word = "train"
+import random
+words = ["education", "stranger", "magazine", "adaptation"]
+
+
 def hangman_greeting():
     print("HANGMAN.")
     print("The game will be available soon.")
 
 
 def simplified_version():
-    print("HANGMAN.")
-    print("Guess the word: >", word)
-    answer = (input("> ")).lower()
-    if answer == word:
-        print("You survived!")
-    else:
-        print("You lost!")
+    word = "train"
+    are_we_playing = True
+    while are_we_playing:
+        answer = (input("Guess the word: > ")).lower()
+        if answer == word:
+            print("You survived!")
+            are_we_playing = False
+        else:
+            print("You lost!")
+            continue
+
+
+def advanced_version():
+    are_we_playing = True
+    word = random.choice(words)
+
+    while are_we_playing:
+        # print(word)
+        answer = (input("Guess the word: > ")).lower()
+        if answer == word:
+            print("You survived!")
+            are_we_playing = False
+        else:
+            print("You lost!")
+            continue
 
 
 hangman_greeting()
-simplified_version()
+
+# simplified_version()
+
+advanced_version()
+
