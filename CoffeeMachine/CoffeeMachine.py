1701708dcbf8e9a5cb2b75eb015849d1e9a9819a
shevchenkoAndriy <s.s.student.khai.edu>
2021-11-06 00:31:11 +0200

    CoffeeMachine 4-st

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -2,11 +2,14 @@ import math
 
 
 class CoffeeMachine:
+    action = ("buy", "fill", "take")
     ingredientsForOne = {"water": 200,
                          "milk": 50,
                          "coffee": 15}
 
-    drinks = ("espresso", "cappuccino", "latte")
+    espresso_action = (250, 16, 1)
+    cappuccino_action = (350, 75, 20, 1)
+    latte_action = (200, 100, 12, 1)
     cooking_stages = '''
     Starting to make a coffee
     Grinding coffee beans
@@ -15,6 +18,13 @@ class CoffeeMachine:
     Pouring coffee into the cup
     Pouring some milk into the cup
     Coffee is ready!'''
+
+    def __init__(self, water=400, milk=540, coffee_beans=120, cups=9, money=550):
+        self.water = water
+        self.milk = milk
+        self.coffee_beans = coffee_beans
+        self.cups = cups
+        self.money = money
 
     def stages(self):
         print(self.cooking_stages)
@@ -46,6 +56,47 @@ class CoffeeMachine:
                   f" (and even {max_cups_coffee - coffee_amount} more than that)")
         else:
             print(f"No, I can make only {max_cups_coffee} cups of coffee.")
+
+    def availability(self):
+        return f"""The coffee machine has:
+        {self.water} of water
+        {self.milk} of milk
+        {self.coffee_beans} of coffee beans
+        {self.cups} of disposable cups
+        {self.money} of money"""
+
+    def calc_max_coffee_cups(self, action_coffee):
+        water = self.water
+        milk = self.milk
+        coffee_beans = self.coffee_beans
+        # cups = self.cups
+        if action_coffee == "1":
+            necessary_water = 250
+            necessary_coffee_beans = 16
+            max_coffee = calc_max_prod(coffee_beans, necessary_coffee_beans)
+            # print(calc_max_prod(coffee_beans, necessary_coffee_beans))
+            max_water = calc_max_prod(water, necessary_water)
+            # print(min(max_coffee, max_water))
+            return min(max_coffee, max_water)
+
+        elif action_coffee == "2":
+            necessary_water = 350
+            necessary_milk = 75
+            necessary_coffee_beans = 20
+            max_milk = calc_max_prod(milk, necessary_milk)
+            max_coffee = calc_max_prod(coffee_beans, necessary_coffee_beans)
+            max_water = calc_max_prod(water, necessary_water)
+
+            return min(max_milk, max_coffee, max_water)
+
+        elif action_coffee == "3":
+            necessary_water = 200
+            necessary_milk = 100
+            necessary_coffee_beans = 12
+            max_milk = calc_max_prod(milk, necessary_milk)
+            max_coffee = calc_max_prod(coffee_beans, necessary_coffee_beans)
+            max_water = calc_max_prod(water, necessary_water)
+            return min(max_milk, max_coffee, max_water)
 
 
 """
@@ -85,13 +136,88 @@ def calc_max_prod(ingredient, ingredients_required_for_one):
     return math.trunc(ingredient / ingredients_required_for_one)
 
 
+def correctly_input_command(string, command):
+    user_input = input(string)
+    while user_input not in command:
+        print("please input correctly command")
+        user_input = input(string)
+    return user_input
+
+
 # cm1 = CoffeeMachine()
 # cm1.stages()
 
 
-cm2 = CoffeeMachine()
-cm2.calc_ingredients()
+# cm2 = CoffeeMachine()
+# cm2.calc_ingredients()
 
 
-cm3 = CoffeeMachine()
-cm3.cooking_possibility_check()
+# cm3 = CoffeeMachine()
+# cm3.cooking_possibility_check()
+
+
+class CoffeeMachine1 (CoffeeMachine):
+
+    def coffee_machine(self):
+        print(self.availability())
+        action = correctly_input_command("Write action (buy, fill, take): > ", self.action)
+        if action == "buy":
+            self.buy_mode()
+        elif action == "fill":
+            self.fill_mode()
+        elif action == "take":
+            self.take_mod()
+
+    def enable_purchase_mode(self, action):
+        if action == "1":
+            self.water -= 250
+            self.coffee_beans -= 16
+            self.cups -= 1
+            self.money += 4
+        elif action == "2":
+            self.water -= 350
+            self.milk -= 75
+            self.coffee_beans -= 20
+            self.cups -= 1
+            self.money += 7
+        elif action == "3":
+            self.water -= 200
+            self.milk -= 100
+            self.coffee_beans -= 12
+            self.cups -= 1
+            self.money += 6
+
+    def buy_mode(self):
+
+        print(self.availability())
+        action = correctly_input_command("What do you want to buy?"
+                                         " 1 - espresso, 2 - latte, 3 - cappuccino: > ", ("1", "2", "3",))
+        max_cups = self.calc_max_coffee_cups(action)
+        if max_cups < 1:
+            print("Sorry I don't have enough ingredients")
+        elif max_cups >= 1:
+            self.enable_purchase_mode(action)
+
+        self.coffee_machine()
+
+    def fill_mode(self):
+        water_add = correct_float_input("Write how many ml of water you want to add: > ")
+        milk_add = correct_float_input("Write how many ml of milk you want to add: > ")
+        coffee_add = correct_float_input("Write how many grams of coffee beans you want to add: > ")
+        cups_add = correct_float_input("Write how many disposable coffee cups you want to add: > ")
+        self.water += water_add
+        self.milk += milk_add
+        self.coffee_beans += coffee_add
+        self.cups += cups_add
+        self.coffee_machine()
+
+    def take_mod(self):
+        revenue = self.money
+        self.money -= revenue
+        print(f"I gave you {revenue}")
+        self.coffee_machine()
+
+
+cm3 = CoffeeMachine1(water=400, milk=540, coffee_beans=120, cups=9, money=550)
+cm3.coffee_machine()
+
