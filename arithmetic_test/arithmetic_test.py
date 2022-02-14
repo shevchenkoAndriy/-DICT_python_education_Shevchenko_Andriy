afbda59dca6016f68fbf4a7c4af0ab460701d270
shevchenko-andrey <s.s.andriy@student.khai.edu>
2022-02-14 22:45:26 +0200

    add def run_test - 2 st

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -7,7 +7,7 @@ def simple_test():
     print(f"{first_number} {action} {second_number} = ")
     user_answer = correct_input_answer("Please input your answer > ")
     true_answer = calc_true_answer(first_number, second_number, action)
-    is_correct_answer(user_answer, true_answer)
+    return is_correct_answer(user_answer, true_answer)
 
 
 def generator(a, b, options):
@@ -44,8 +44,21 @@ def calc_true_answer(first_number, second_number, action):
 def is_correct_answer(answer, true_answer):
     if answer == true_answer:
         print("Right!")
+        return 1
     else:
         print("Wrong!")
+        return 0
 
 
-simple_test()
+def run_test(iteration_number, test):
+    correct_answer = 0
+    counter = 1
+    while counter <= iteration_number:
+        correct_answer += test()
+        counter += 1
+
+    print(f"Your mark is {correct_answer}/{iteration_number}.")
+
+
+# simple_test() #st-1
+run_test(5, simple_test)
