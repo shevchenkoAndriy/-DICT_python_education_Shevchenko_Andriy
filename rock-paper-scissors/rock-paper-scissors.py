f48bfc4020f26b4593be2bdcb099a60dfb689b3c
shevchenko-andrey <s.s.andriy@student.khai.edu>
2022-03-28 17:54:26 +0300

    step-2

--- rock-paper-scissors/rock-paper-scissors.py
+++ rock-paper-scissors/rock-paper-scissors.py
@@ -1,3 +1,6 @@
+import random
+
+
 def simple_version():
     user_choice = correct_input_action("Your choice > ", ("rock", "paper", "scissors"))
     win_move = search_win_move(user_choice)
@@ -23,5 +26,27 @@ def correct_input_action(string, valid_commands):
     return user_input
 
 
+def simple_menu():
+    user_choice = correct_input_action("Your choice > ", ("rock", "paper", "scissors", "exit"))
+
+    if user_choice == "exit":
+        print("Bye")
+        return
+
+    computer_choice = random.choice(("rock", "paper", "scissors"))
+    computer_win_if = search_win_move(user_choice)
+    user_win_if = search_win_move(computer_choice)
+
+    if computer_win_if == computer_choice:
+        print(f"Sorry, but the computer chose {computer_choice}")
+
+    elif user_win_if == user_choice:
+        print(f"Well done. The computer chose {computer_choice} and failed")
+
+    simple_menu()
+
+
 """step-1"""
-simple_version()
+# simple_version()
+"""step-2"""
+simple_menu()
