faa60e4c19689a0c6499cf58a807b3c66d411351
shevchenko-andrey <s.s.andriy@student.khai.edu>
2022-04-07 23:37:38 +0300

    step-2

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -7,6 +7,15 @@ def simple_currency_exchange():
     print(f"The total amount of dollars: {round(coins * exchange_rate, 2)}")
 
 
+def show_currency_exchange():
+    coin = correct_float_input("Please, enter the number "
+                               "of coins you have: > ")
+    print(f"I will get {round(coin * 2.87)} ARS from the sale of {coin} coin")
+    print(f"I will get {round(coin * 0.6)} HNL from the sale of {coin} coin")
+    print(f"I will get {round(coin * 6.87)} AUD from the sale of {coin} coin")
+    print(f"I will get {round(coin * 0.73)} MAD from the sale of {coin} coin")
+
+
 def correct_float_input(string):
     user_input = input(string)
     while True:
@@ -24,4 +33,5 @@ def correct_float_input(string):
     return float(user_input)
 
 
-simple_currency_exchange()
+# simple_currency_exchange()
+show_currency_exchange()
