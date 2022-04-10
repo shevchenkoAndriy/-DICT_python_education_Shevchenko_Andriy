b6629b5712c7fb07d8b0ffa5886315796f975840
shevchenko-andrey <s.s.andriy@student.khai.edu>
2022-04-10 22:11:26 +0300

    step-4

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -1,71 +1,78 @@
 import requests
 import json
-from functools import lru_cache
 
 
-def simple_currency_exchange():
-    coins = correct_float_input("Please, enter the number "
-                                "of coins you have: > ")
-    exchange_rate = correct_float_input("Please, enter the exchange rate: > ")
+class CurrencyExchange:
 
-    print(f"The total amount of dollars: {round(coins * exchange_rate, 2)}")
+    def __init__(self):
+        self.cache = dict()
+        self.currency_for_exchange = None
 
+    def check_cache(self, func, currency):
 
-def show_currency_exchange():
-    coin = correct_float_input("Please, enter the number "
-                               "of coins you have: > ")
-    print(f"I will get {round(coin * 2.87)} ARS from the sale of {coin} coin")
-    print(f"I will get {round(coin * 0.6)} HNL from the sale of {coin} coin")
-    print(f"I will get {round(coin * 6.87)} AUD from the sale of {coin} coin")
-    print(f"I will get {round(coin * 0.73)} MAD from the sale of {coin} coin")
+        print("Checking the cache...")
+        if currency in self.cache:
+            print("It is in the cache!")
+            return self.cache[currency]
 
+        print("Sorry, but it is not in the cache!")
+        result = func(currency)
+        self.cache[currency] = result
 
-@lru_cache(maxsize=100)
-def get_exchange_rate(code):
-    print(f"Search... ")
-    response = requests.get(f"http://www.floatrates.com/daily/{code}.json")
-    if response.ok:
-        result = response.json()
-        d = result.get("usd").get("rate")
-        e = result.get("eur").get("rate")
-        return round(d, 2), round(e, 2)
+        return result
 
-    else:
-        return
+    def get_exchange_rate(self, currency):
+        print(f"Wait... ")
+        response = requests.get(f"http://www.floatrates.com/daily/{self.currency_for_exchange}.json")
+        if response.ok:
+            result = response.json()
 
+            try:
 
-def step_3():
-    code = input("> ").lower()
-    results = get_exchange_rate(code)
-    if not results:
-        print(f'No results found for "{code}"')
-        step_3()
-        return
+                result = result.get(f"{currency}").get("rate")
+            except AttributeError:
+                return
 
-    dollar, euro = results
-    print(f"""Dollar: {round(dollar, 2)}
-Euro: {round(euro, 2)}""")
-    step_3()
-    ...
+            return round(result, 2)
 
-
-def correct_float_input(string):
-    user_input = input(string)
-    while True:
-        try:
-            float(user_input)
-        except ValueError:
-            if "," in user_input:
-                user_input = user_input.replace(",", ".")
-                continue
-            print("Incorrect format")
-            user_input = input(string)
-            continue
         else:
-            break
-    return float(user_input)
+            return
+
+    def menu(self):
+        self.currency_for_exchange = input("Currency for exchange > ").lower()
+        self.play_currency_exchange()
+
+    def play_currency_exchange(self):
+        amount_of_many = self.correct_float_input("Amount of many > ")
+        currency = input("What to change > ").lower()
+        exchange_rate = self.check_cache(self.get_exchange_rate, currency)
+        if not exchange_rate:
+            print("Incorrect parameters")
+            self.play_currency_exchange()
+            return
+        print(f"You received {round(amount_of_many * exchange_rate, 2)} {currency.upper()}.")
+        self.play_currency_exchange()
+
+    @staticmethod
+    def correct_float_input(string):
+        user_input = input(string)
+        while True:
+            try:
+                float(user_input)
+            except ValueError:
+                if "," in user_input:
+                    user_input = user_input.replace(",", ".")
+                    continue
+                print("Incorrect format")
+                user_input = input(string)
+                continue
+            else:
+                break
+        return float(user_input)
 
 
 # simple_currency_exchange()
 # show_currency_exchange()
-step_3()
+# step_3()
+currency_ex = CurrencyExchange()
+currency_ex.menu()
