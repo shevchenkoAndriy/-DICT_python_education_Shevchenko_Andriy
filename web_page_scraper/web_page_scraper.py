2e6e9bb71ab6e4a5a2befcd84d03533912002360
shevchenko-andrey <s.s.andriy@student.khai.edu>
2022-05-02 17:54:44 +0300

    step-3

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -31,6 +31,21 @@ def parse_html_doc():
         print("Invalid movie page!")
 
 
+def save_html_page():
+    url = correct_input_url("Please input URL > ")
+    response = requests.get(url)
+    if response.ok:
+        save_in_file(response.content)
+    else:
+        print(f"The URL returned {response.status_code}")
+
+
+def save_in_file(page):
+    with open("web_page.html", "wb") as f:
+        f.write(page)
+        print("Content saved.")
+
+
 def correct_input_url(string):
     url = input(string)
     if url.startswith('http://'):
@@ -42,4 +57,5 @@ def correct_input_url(string):
 
 
 # simple_parser()
-parse_html_doc()
+# parse_html_doc()
+save_html_page()
