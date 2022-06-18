ee51604cfe5272398af3d6d7178016c19b7abd81
andrew <s.s.student@khai.edu>
2022-06-18 20:14:34 +0300

    step 2

--- regular_expressions/regular_expressions.py
+++ regular_expressions/regular_expressions.py
@@ -1,5 +1,16 @@
-def simple_reg_ex_parser():
-    req_ex, string = input("Please input regular_expressions|your string > ").split("|")
+def correct_input_reg_ex():
+    user_input = input("Please input regular_expressions|your string > ")
+    while True:
+        try:
+            req_ex, string = user_input.split("|")
+            return req_ex, string
+        except ValueError:
+            print("Incorrect format")
+            user_input = input("Please input regular_expressions|your string > ")
+            continue
+
+
+def simple_reg_ex_parser(req_ex, string):
     if req_ex == "":
         return True
     elif string == "":
@@ -7,9 +18,38 @@ def simple_reg_ex_parser():
 
     elif req_ex == ".":
         return True
+
     else:
-        return req_ex == string
+        return True
 
 
-print(simple_reg_ex_parser())
+def complicated_parser(req_ex, string):
+    if len(req_ex) != len(string):
+        return False
 
+    for r, s in zip(req_ex, string):
+        if r == ".":
+            continue
+        elif r != s:
+            return False
+
+    return True
+
+
+def main():
+    req_ex, string = correct_input_reg_ex()
+    first_check_level = simple_reg_ex_parser(req_ex, string)
+
+    if not first_check_level:
+        print(False)
+        return
+
+    second_check_level = complicated_parser(req_ex, string)
+    if not second_check_level:
+        print(False)
+        return
+
+    print(True)
+
+
+main()
