cdd0e7b608c95a75c7c3e6f54fd8f27989c78be3
andrew <s.s.student@khai.edu>
2022-06-18 22:19:19 +0300

    step 3

--- regular_expressions/regular_expressions.py
+++ regular_expressions/regular_expressions.py
@@ -22,10 +22,31 @@ def simple_reg_ex_parser(req_ex, string):
     else:
         return True
 
+def equal_len(regex: str, literal: str) -> bool:
+    if regex == "":
+        return True
+    elif literal == "":
+        return False
+    elif regex[0] != "." and regex[0] != literal[0]:
+        return False
+    else:
+        return equal_len(regex[1:], literal[1:])
+
+
+def different_len(regex: str, literal: str) -> bool:
+    equal_len_matches: bool = equal_len(regex, literal)
+
+    if equal_len_matches:
+        return True
+    elif literal == "":
+        return False
+    else:
+        return different_len(regex, literal[1:])
+
 
 def complicated_parser(req_ex, string):
     if len(req_ex) != len(string):
-        return False
+        return True
 
     for r, s in zip(req_ex, string):
         if r == ".":
@@ -46,6 +67,12 @@ def main():
 
     second_check_level = complicated_parser(req_ex, string)
     if not second_check_level:
+        print(False)
+        return
+
+    third_check_level = different_len(req_ex, string)
+
+    if not third_check_level:
         print(False)
         return
 
